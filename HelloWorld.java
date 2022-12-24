1373ef9f59c5655f4d579bb6fc1d3413c517f008
naziakhanee <121333430+naziakhanee@users.noreply.github.com>
2022-12-24 07:23:04 +0000

    add program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,18 @@
 package com.study;
 
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String args[]){
-        System.out.println("This is a test");
+        Scanner sc = new Scanner();
+        System.out.println("Enter first number ");
+        int num1 = Integer.parse(sc.nextLine());
+
+        System.out.println("Enter second number ");
+        int num2 = Integer.parse(sc.nextLine());
+
+
+        System.out.println("Sum of number is " + (num1 + num2));
+
     }
 }
